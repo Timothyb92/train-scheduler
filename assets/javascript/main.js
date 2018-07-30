c9cd9fc10103d2fbd13dad52da6737fdea2877c2
Timothy Bedsole <timbedsole@gmail.com>
2018-07-30 18:54:11 -0400

    Added link in js file to firebase db. Created base layout of html

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
-    
+    var config = {
+        apiKey: "AIzaSyCb_czrT2veSzGMH7XJTCu3VdT0zo2kxdw",
+        authDomain: "best-ever-8ec0f.firebaseapp.com",
+        databaseURL: "https://best-ever-8ec0f.firebaseio.com",
+        projectId: "best-ever-8ec0f",
+        storageBucket: "best-ever-8ec0f.appspot.com",
+        messagingSenderId: "853031341422"
+    };
+    firebase.initializeApp(config);
 })
