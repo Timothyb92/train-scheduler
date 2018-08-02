3d74de22c75b17d0f7f0e45b28143eb0dc7081b5
Timothy Bedsole <timbedsole@gmail.com>
2018-08-02 16:46:26 -0400

    changed variables to facilitate moment formatting. getting NaN returned and not sure why

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -9,28 +9,51 @@ $(document).ready(function(){
       };
       firebase.initializeApp(config);
 
-    var db = firebase.database();
-    var trainName = $("#trainName").val().trim();
-    var trainDestination = $("#destination").val().trim();
-    var trainFirstTime = $("#firstTrainTime").val().trim();
-    var trainFreq = $("#frequency").val().trim();
-    var minsAway;
+      var db = firebase.database();
+      
+      var trainFirstTime = $("#firstTrainTime").val().trim();
+      var trainFreq = $("#frequency").val().trim();
+      var trainName = $("#trainName").val().trim();
+      var trainDestination = $("#destination").val().trim();
+      var firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+      var timeDiff = moment().diff(moment(firstTimeConverted), "minutes");
+      var remainder = timeDiff % trainFreq;
+      var minutesTillTrain = trainFreq - remainder;
+      var nextTrain = moment().add(minutesTillTrain, "minutes");
+
+
+
+
+
+    // var trainFirstTime = $("#firstTrainTime").val().trim();
+    // var trainFreq = $("#frequency").val().trim();
+    // var minsAway;
     // var trainTimeConverted = moment($("#firstTrainTime").val(), "HH:mm").format("X");
-    var trainTimeConverted = moment(trainFirstTime, "HH:mm").subtract(1, "years");
+    // var trainTimeConverted = moment(trainFirstTime, "HH:mm").subtract(1, "years");
     // var trainTimeFormat = moment.unix(trainTimeConverted).format("h:mma");
-    var timeDiff = moment().diff(moment(trainTimeConverted), "minutes")
+    // var timeDiff = moment().diff(moment(trainTimeConverted), "minutes")
     // console.log(timeDiff);
 
 
     $("#addTrain").click(function(event){
         event.preventDefault();
-        var newTrain = {
-            name: trainName,
-            destination: trainDestination,
-            frequency: trainFreq,
-            firstTime: trainFirstTime,
+        // var newTrain = {
+        //     name: trainName,
+        //     destination: trainDestination,
+        //     frequency: trainFreq,
+        //     firstTime: trainFirstTime,
 
-        };
+        // };
+        // console.log("Next train: " + nextTrain);
+        // console.log("First time converted: " + firstTimeConverted);
+        // console.log("time diff: " + timeDiff);
+        console.log("first train time: ");
+        console.log(trainFirstTime);
+        // console.log();
+        // console.log();
+        // console.log();
+        // console.log();
+        
     })
 
 
